b67b211d12b1404c57116a439c4b98fc6cd5101b
sahil kumrawat <123446028+sahilKumrawat@users.noreply.github.com>
2025-02-01 05:46:37 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const ProductRouter = require('./Routes/ProductRouter');
 
 require('dotenv').config();
 require('./Models/db');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
     res.send('PONG');
