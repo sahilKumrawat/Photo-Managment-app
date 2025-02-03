7fdfb39fbdf560edf885ad71f65e112f7a3abe4d
sahil kumrawat <123446028+sahilKumrawat@users.noreply.github.com>
2025-02-02 23:22:51 -0800

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -6,6 +6,9 @@ import { removeUser, } from '../currentUserSlice';
 import { useSelector, useDispatch } from 'react-redux';
 import { CgEnter, CgProfile } from "react-icons/cg";
 import { FaSearch } from "react-icons/fa";
+import { MdLogout } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
+import { MdNavigateNext } from "react-icons/md";
 
 function Home() {
 
@@ -14,7 +17,7 @@ function Home() {
     const dispatch = useDispatch()
     const userData = useSelector((state) => state.currentUser)
     const [userList, setUserList] = useState([])
-    const [limit, setLimit] = useState(3)
+    const [limit, setLimit] = useState(6)
     const [currentPage, setCurrentPage] = useState(1)
     const [search, setSearch] = useState('')
     const [searchedObj, setSearchedObj] = useState({})
@@ -22,8 +25,9 @@ function Home() {
     const lastIndex = (limit * currentPage)
     const startIndex = (lastIndex - limit)
 
-    const style = { display: 'inline-block', marginLeft: '150px', padding: '20px', justifyContent: 'center' }
-    const styleCard = { display: 'inline-block', marginLeft: '150px', padding: '20px', justifyContent: 'center', border: "2px solid white", borderRadius: "38px", backgroundColor: "#0f0c0c" }
+
+    const style = { display: 'inline-block', padding: '20px', borderRadius: "38px", boxShadow: "5px 5px 5px 5px rgb(219, 221, 221)", width: "200px", margin: "20px" }
+    const styleCard = { display: 'inline-block', padding: '20px', border: "2px solid white", borderRadius: "38px", backgroundColor: "#196c6c", width: "200px", margin: "10px" }
 
 
 
@@ -71,6 +75,16 @@ function Home() {
                     item['flag'] = true
                 }
             })
+        } if (e.target.value === 'prime') {
+            tempList.forEach(item => {
+                for (let i = 2; i <= Math.sqrt(item.id); i++) {
+                    if (item.id % i === 0) {
+                        return
+                    }
+                }
+                if (item.id !== 1) item['flag'] = true
+            })
+
         }
         setUserList(tempList)
     }
@@ -84,7 +98,7 @@ function Home() {
     }
 
     const handleSearch = () => {
-        let obj = userList.find(item => item.id == Number(search))
+        let obj = userList.find(item => item.name.toLowerCase() === search.toLowerCase())
         if (!obj) {
             setSearch('')
             setSearchedObj({})
@@ -99,69 +113,83 @@ function Home() {
     let chunkList = userList.slice(startIndex, lastIndex)
     console.log("chunkList ", chunkList)
 
-    return (<>
-        <div style={{
-            backgroundColor: "gray"
-        }}>
-            <span style={{ marginRight: '800px' }}> Photo Managment  </span>
-            <span>
-                <select onClick={handleSelect} defaultValue="select">
-                    <option value="select">select</option>
-                    <option value="odd">Odd</option>
-                    <option value="even">Even</option>
-                </select>
-            </span>
-            <span><CgProfile /> {loggedInUser}</span>
-            <span><button onClick={handleLogout}>Logout</button></span>
-        </div>
-        <div className='home-div'>
-            <div style={{ marginLeft: "37%", marginTop: "14px" }}>
-                <input style={{ height: "40px", width: '30%', border: "2px solid white", borderRadius: '19px', padding: "8px" }} type="text"
-                    onChange={(e) => setSearch(e.target.value)} value={search} placeholder='Enter Album Id' />
-                <button style={{ marginLeft: "5px" }} onClick={handleSearch}><FaSearch /></button>
-            </div >
-
-            {searchedObj?.name ?
-                <div style={{ marginTop: "60px" }}>
-                    <div style={styleCard} key={searchedObj.id}>
-                        <img src='../../public/1.png' alt="Photo" />
-                        <p>{searchedObj.name}</p>
-                        <p style={{ backgroundColor: "gray", borderRadius: '6px', textAlign: 'center' }}>Album : {searchedObj.id}</p>
+    return (
+        <>
+            <div className='main-div'>
+                <div className='first-fram-div'></div>
+                <div className='second-fram-div'>
+                    <div className='header-div'>
+                        <span> <h2 className='custom-h2'>Photo Managment </h2>  </span>
+                        <span className='header-profile'><CgProfile size={25} /> <label htmlFor="" className='user-label'>{loggedInUser}</label><button className='logout-button' onClick={handleLogout}><MdLogout size={25} /></button></span>
                     </div>
-                </div >
-                :
+                    <div className='feature-div'>
+                        <span >
 
-                <div style={{ marginTop: "60px" }}>
-                    {
-                        chunkList && chunkList?.map((item, index) => (
-                            <div style={(item.flag) ? styleCard : style} key={index}>
-                                <img src='https://github.com/sahilKumrawat/Photo-Managment-app/blob/master/frontend/public/1.png?raw=true' alt="Photo" />
-                                <p>{item.name}</p>
-                                <p style={{ backgroundColor: "gray", borderRadius: '6px', textAlign: 'center' }}>Album : {item.id}</p>
+                            <input className='search-input-custom' type="text"
+                                onChange={(e) => setSearch(e.target.value)} value={search} placeholder='Search by name' />
+                            <button className='search-button' onClick={handleSearch}><FaSearch /></button>
+
+                            <select className='custom-select' onClick={handleSelect} defaultValue="select">
+                                <option value="select">select</option>
+                                <option value="odd">Odd</option>
+                                <option value="even">Even</option>
+                                <option value="prime">Prime</option>
+                            </select>
+                        </span >
+                    </div>
+
+
+                    {searchedObj?.name ?
+                        <div style={{ marginTop: "60px" }}>
+                            <div style={styleCard} key={searchedObj?.id}>
+                                <img className='profile-img' src='https://github.com/sahilKumrawat/Photo-Managment-app/blob/master/frontend/public/1.png?raw=true' alt="Photo" />
+                                <p style={{ textAlign: 'center', fontSize: "large" }}>{searchedObj?.name}</p>
+                                <p className='album-p'>Album : {searchedObj?.id}</p>
+                                <p className='email-p'>{searchedObj?.email}</p>
                             </div>
-                        ))
-                    }
-                </div>
-            }
 
-            <ToastContainer />
-        </div>
-        <div style={{ position: 'fixed', bottom: '0', right: '0' }}>
-            {searchedObj.name ?
-                <>
-                <button  onClick={() => {setSearchedObj({}), setSearch("")}}>Back </button>
-                </>
-                :
-                <>
-                    <button disabled={currentPage === 1} onClick={() => handlePrevious()}>Previous</button>
-                    {Array.from({ length: totalPage }, (_, index) => (
-                        <button key={index} style={(currentPage === index + 1) ? { border: "2px solid white", borderRadius: "5px" } : {}} onClick={() => setCurrentPage(index + 1)}>{index + 1}</button>
-                    ))}
-                    <button disabled={currentPage === totalPage} onClick={() => handleNext()}>Next</button>
-                </>
-            }
-        </div >
-    </>
+                        </div >
+                        :
+
+                        <div style={{ marginTop: "60px" }}>
+                            {
+                                chunkList && chunkList?.map((item, index) => (
+                                    <div style={(item.flag) ? styleCard : style} key={index}>
+                                        <img className='profile-img' src='https://github.com/sahilKumrawat/Photo-Managment-app/blob/master/frontend/public/1.png?raw=true' alt="Photo" />
+                                        <p style={{ textAlign: 'center', fontSize: "large" }}>{item.name}</p>
+                                        <p className='album-p'>Album : {item.id}</p>
+                                        <p className='email-p'>{item.email}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    }
+
+                    <ToastContainer />
+
+                    <div className='footer-div'>
+                        {searchedObj.name ?
+                            <>
+                                <button onClick={() => { setSearchedObj({}), setSearch("") }}>Back </button>
+                            </>
+                            :
+                            <>
+                                <div className='page-div'>Showing {chunkList[0]?.id} to {chunkList[chunkList.length-1]?.id} of {userList.length} entries</div>
+                                <div>
+                                    <button className='pagination-button' disabled={currentPage === 1} onClick={() => handlePrevious()}><GrFormPrevious /></button>
+                                    {Array.from({ length: totalPage }, (_, index) => (
+                                        <button className='pagination-button' key={index} style={(currentPage === index + 1) ? { backgroundColor: " #196c6c", color: "white" } : {}} onClick={() => setCurrentPage(index + 1)}>{index + 1}</button>
+                                    ))}
+                                    <button className='pagination-button' disabled={currentPage === totalPage} onClick={() => handleNext()}><MdNavigateNext /></button>
+                                </div>
+                            </>
+                        }
+                    </div >
+
+                </div>
+
+            </div>
+        </>
 
     )
 }
